9c547c84d497430ddd664d4629db3a6134fd90f9
Siarhei Luskanau <siarhei.luskanau@gmail.com>
2019-11-23 09:08:39 +0300

    Removed spinner from DriverStartRoute screen

--- network/src/main/java/com/github/yarohovichalex/marshruber/android/network/storage/MurshruberStorageApiImpl.kt
+++ network/src/main/java/com/github/yarohovichalex/marshruber/android/network/storage/MurshruberStorageApiImpl.kt
@@ -6,14 +6,12 @@ import android.preference.PreferenceManager
 import com.github.yarohovichalex.marshruber.android.common.MarshruberStorageApi
 import com.github.yarohovichalex.marshruber.android.common.data.DriverData
 import com.github.yarohovichalex.marshruber.android.common.data.RiderData
-import com.github.yarohovichalex.marshruber.android.network.data.DriverDto
-import com.github.yarohovichalex.marshruber.android.network.data.RouteDto
 
-class MurshruberStorageApiImpl (
-    context:Context
+class MurshruberStorageApiImpl(
+    context: Context
 ) : MarshruberStorageApi {
 
-    val sharedPreferences: SharedPreferences = PreferenceManager.getDefaultSharedPreferences(context);
+    val sharedPreferences: SharedPreferences = PreferenceManager.getDefaultSharedPreferences(context)
 
     override suspend fun getDriverStorage(): DriverData? {
 //        val driver = sharedPreferences.getString("driver", "driver")
@@ -27,10 +25,10 @@ class MurshruberStorageApiImpl (
     }
 
     override suspend fun getRiderStorage(): DriverData? {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        TODO("not implemented") // To change body of created functions use File | Settings | File Templates.
     }
 
     override suspend fun putRiderStorage(rider: RiderData) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        TODO("not implemented") // To change body of created functions use File | Settings | File Templates.
     }
 }
